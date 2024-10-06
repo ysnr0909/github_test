7c179bb9649409c9bb2f6085279cc70f166517f5
ysnr0909 <ysnr.suzuki@gmail.com>
2024-10-06 12:01:23 +0000

    厳密等価演算子(===)を使用するように修正

--- sort.js
+++ sort.js
@@ -1,9 +1,9 @@
 var sortNumber = function (number) {
    number.sort(function (a, b) {
-       if (a == b) {
-           return 0;
-       }
-       return a < b ? -1 : 1;
+	   if (a === b) {
+		   return 0;
+	   }
+	   return a < b ? -1 : 1;
    });
 };
 
